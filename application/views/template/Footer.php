4f8c49cb61343622e2d7c2f27b8d8dd87abed574
savirajatnika <deuisnjlx@gmail.com>
2017-10-08 12:18:49 +0700

    Add files via upload

---
+++ application/views/template/Footer.php
@@ -0,0 +1,13 @@
+<footer class="w3-center w3-light-grey w3-padding-32">
+  <p>Powered by <a href="#" title="" target="_blank" class="w3-hover-text-green">P O L A L O Y D TEAM </a></p>
+</footer>
+
+<script src="<?php echo base_url()?>assets/bootstrap-assets/js/jquery.js"></script>
+    <script src="<?php echo base_url()?>assets/bootstrap-assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url()?>assets/bootstrap-assets/js/bootstrap.js"></script>
+    <script src="<?php echo base_url()?>assets/bootstrap-assets/js/npm.js"></script>
+    <script src="<?php echo base_url()?>assets/js/index.js"></script>
+    <script src="<?php echo base_url()?>assets/js/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/custom.js"></script>
+    <script src="<?php echo base_url()?>assets/gallery/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url()?>assets/gallery/vendor/bootstrap/js/bootstrap.min.js"></script>
