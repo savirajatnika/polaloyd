9da92ac640fc3004c7c14bd57853fe8bdd2cdfbb
savirajatnika <deuisnjlx@gmail.com>
2017-10-24 19:44:29 +0700

    Update polaloyd.sql
    sql

--- polaloyd.sql
+++ polaloyd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2017 at 03:13 PM
+-- Generation Time: Oct 24, 2017 at 02:19 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`username`, `password`) VALUES
-('admin', '123');
+('admin', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ CREATE TABLE `gallery` (
 --
 
 INSERT INTO `gallery` (`ID_gallery`, `caption_gallery`, `occasion_gallery`, `gambar_gallery`, `keterangan_gallery`) VALUES
-(13, 'tes1', 'tes2', 'uploads/pol2.jpg', 'tes3');
+(33, 'Polaroid', 'Birthday', 'uploads/18f08bdebf15580888ec4db8d95fd240.jpg', 'Nice');
 
 -- --------------------------------------------------------
 
@@ -72,6 +72,16 @@ CREATE TABLE `komentar` (
   `No_Telp` varchar(50) NOT NULL,
   `Message` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `komentar`
+--
+
+INSERT INTO `komentar` (`ID_komentar`, `Name`, `Email`, `No_Telp`, `Message`) VALUES
+(238, 'ajahdjaba', 'bdaafac', 'cadacaca', 'ddadacacacavaaafafafafafaaababafnjayda'),
+(239, 'ajahdjaba', 'bdaafac', 'cadacaca', 'ddadacacacavaaafafafafafaaababafnjayda'),
+(240, 'a', 'a@gmail.com', '0002010221912', 'fafas'),
+(241, 'create', 'create@gmail.com', '08818181811', 'coba create');
 
 -- --------------------------------------------------------
 
@@ -90,7 +100,7 @@ CREATE TABLE `slider` (
 --
 
 INSERT INTO `slider` (`ID_slider`, `caption_slider`, `gambar_slider`) VALUES
-(11, 'tes', 'uploads/pol1.jpg');
+(33, 'Polaroid', 'uploads/18f08bdebf15580888ec4db8d95fd240.jpg');
 
 --
 -- Indexes for dumped tables
@@ -122,17 +132,17 @@ ALTER TABLE `slider`
 -- AUTO_INCREMENT for table `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `ID_gallery` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `ID_gallery` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 --
 -- AUTO_INCREMENT for table `komentar`
 --
 ALTER TABLE `komentar`
-  MODIFY `ID_komentar` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_komentar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=242;
 --
 -- AUTO_INCREMENT for table `slider`
 --
 ALTER TABLE `slider`
-  MODIFY `ID_slider` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `ID_slider` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
